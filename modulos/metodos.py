bfc7523ac503ea2f180ebbbfde4f98dc7782a837
PutoyJJ14 <lopezjj@uamv.edu.ni>
2025-04-27 17:14:28 -0600

    Se agrega el metodo insertar paciente

--- modulos/metodos.py
+++ modulos/metodos.py
@@ -1,4 +1,12 @@
 import clases # importe de las clases creadas 
 
 # metodo para insertar un paciente 
-
+def insertar_paciente(lista, nombre, edad, sintoma, prioridad): # recibe la lista y los datos del paciente 
+    nuevo_paciente = clases.Paciente(nombre, edad, sintoma, prioridad) # crea el nuevo paciente 
+    if lista.primero is None: # si la lista esta vacia 
+        lista.primero = nuevo_paciente # el nuevo paciente es el primero 
+    else: # si no esta vacia 
+        actual = lista.primero # empieza desde el primero 
+        while actual.siguiente is not None: # mientras haya pacientes en la lista 
+            actual = actual.siguiente # avanza al siguiente paciente 
+        actual.siguiente = nuevo_paciente # agrega el nuevo paciente al final de la lista
