7fa80ce729147d40263bc75cf01f2956247741f9
Lopez <168609539+SrLopezR@users.noreply.github.com>
2025-05-13 13:47:26 -0600

    Agregando carpeta de trabajo de ejercicios de cola

---
+++ Ejercicios de Cola/EJ Clinica.py
@@ -0,0 +1,6 @@
+'''Desarrolle un programa en Python que simule una cola de atención en una clínica. El sistema 
+debe permitir agregar pacientes a la cola (registro de llegada), atender al siguiente paciente 
+(eliminar el primero en la cola) y mostrar en pantalla la lista actual de pacientes en espera. El 
+docente implementará el programa paso a paso, explicando cada parte del código, mientras los 
+estudiantes proponen mejoras, prueban con distintos datos y analizan el comportamiento de la 
+estructura tipo cola.'''
