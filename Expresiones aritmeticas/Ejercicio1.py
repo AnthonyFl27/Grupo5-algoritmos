9cf5b546e35e96b99e1a957d92522b6bb4cd1f15
Lopez <168609539+SrLopezR@users.noreply.github.com>
2025-05-19 21:50:10 -0600

    Creacion de carpeta de trabajo y archivo Ejercicio1.py

---
+++ Expresiones aritmeticas/Ejercicio1.py
@@ -0,0 +1,59 @@
+#invertir palabras en una frase 
+# Ejemplo : "Hola Mundo" -> "Mundo Hola"
+
+class Nodo:
+    def __init__(self, dato):
+        self.dato = dato
+        self.siguiente = None
+
+class Pila:
+    def __init__(self):
+        self.cima = None
+
+    def push(self, dato):
+        nuevo_nodo = Nodo(dato)
+        nuevo_nodo.siguiente = self.cima
+        self.cima = nuevo_nodo
+
+    def pop(self):
+        if self.cima is None:
+            return None
+        dato = self.cima.dato
+        self.cima = self.cima.siguiente
+        return dato
+
+    def cima (self):
+        if self.cima is None:
+            return None
+        return self.cima.dato
+    
+    def imprimir(self):
+        actual = self.cima
+        while actual is not None:
+            print(actual.dato, end="\n")
+            actual = actual.siguiente
+        print()
+    #Definiendo el método separarParImpar
+
+
+def invertir_frase(frase):
+        pila = Pila()
+        palabras = frase.split()
+
+        for palabra in palabras:
+            pila.push(palabra)
+
+        frase_invertida = ""
+        while pila.cima is not None:
+            frase_invertida += pila.pop() + " "
+        return frase_invertida.strip()
+# Pedimos que el usuario ingrese una frase y cada palabra se almacenara como un nodo en la pila
+# Ejemplo de uso
+frase = input("Ingrese una frase: ")
+resultado = invertir_frase(frase)
+print("Frase invertida:", resultado)
+
+
+
+
+
